c312b663d1940ee2ada11ad7d0961bcfcf8d65f2
vicky <titi013139@gmail.com>
2020-06-02 18:43:55 +0800

    Form Reactive
    200 to 209

---
+++ forms-reactive-start/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  genders = ['male', 'female'];
+  signUpForm: FormGroup;
+
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null,Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email])
+      }),
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
+    })
+  }
+
+  onSubmit() {
+    console.log(this.signUpForm);
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signUpForm.get('hobbies')).push(control);
+  }
+  getControls() {
+    return (<FormArray>this.signUpForm.get('hobbies')).controls;
+  }
+}
